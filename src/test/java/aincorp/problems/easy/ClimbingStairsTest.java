4b3bd00fda4f3624d1b95d89656356614b3789d6
Anvarov Almaz <almaz.anvarov@aplana.com>
2021-05-15 22:04:12 +0300

    leetcode-w-test 7 add ClimbingStairs algorithmic problem

--- src/test/java/aincorp/problems/easy/ClimbingStairsTest.java
+++ src/test/java/aincorp/problems/easy/ClimbingStairsTest.java
@@ -7,13 +7,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ClimbingStairsTest {
 
     @Test
-    public void when_steps_three_output_is_three() {
+    public void when_steps_two_output_is_two() {
         ClimbingStairs climbingStairs = new ClimbingStairs();
+        assertEquals(2, climbingStairs.climbStairs(2));
     }
 
     @Test
-    public void when_steps_zero_output_is_zero() {
+    public void when_steps_three_output_is_three() {
         ClimbingStairs climbingStairs = new ClimbingStairs();
-        assertEquals(0, climbingStairs.climbStairs(0));
+        assertEquals(3, climbingStairs.climbStairs(3));
+    }
+
+    @Test
+    public void when_steps_four_output_is_five() {
+        ClimbingStairs climbingStairs = new ClimbingStairs();
+        assertEquals(5, climbingStairs.climbStairs(4));
+    }
+
+    @Test
+    public void when_steps_five_output_is_eight() {
+        ClimbingStairs climbingStairs = new ClimbingStairs();
+        assertEquals(8, climbingStairs.climbStairs(5));
     }
 }
