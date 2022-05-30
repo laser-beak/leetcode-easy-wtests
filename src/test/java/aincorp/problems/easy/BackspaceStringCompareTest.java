37abbe58b74aba5318988a1725ca31dd58a88144
Анваров Алмаз Салимович (4060794) <asanvarov@vtb.ru>
2022-05-30 15:01:02 +0300

    844. Backspace String Compare - fix asserts

--- src/test/java/aincorp/problems/easy/BackspaceStringCompareTest.java
+++ src/test/java/aincorp/problems/easy/BackspaceStringCompareTest.java
@@ -2,7 +2,7 @@ package aincorp.problems.easy;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BackspaceStringCompareTest {
 
@@ -10,27 +10,27 @@ class BackspaceStringCompareTest {
 
     @Test
     public void true_when_strings_empty() {
-        assertEquals(true, backspaceStringCompare.backspaceCompare("", ""));
+        assertTrue(backspaceStringCompare.backspaceCompare("", ""));
     }
 
     @Test
     public void true_when_strings_become_same() {
-        assertEquals(true, backspaceStringCompare.backspaceCompare("ab#c", "ad#c"));
+        assertTrue(backspaceStringCompare.backspaceCompare("ab#c", "ad#c"));
     }
 
     @Test
     public void true_when_strings_become_same_eq_empty() {
-        assertEquals(true, backspaceStringCompare.backspaceCompare("ab##", "c#d#"));
+        assertTrue(backspaceStringCompare.backspaceCompare("ab##", "c#d#"));
     }
 
     @Test
     public void false_when_strings_become_not_same() {
-        assertEquals(false, backspaceStringCompare.backspaceCompare("a#c", "b"));
+        assertFalse(backspaceStringCompare.backspaceCompare("a#c", "b"));
     }
 
     @Test
     public void true_when_empty_stack() {
-        assertEquals(true, backspaceStringCompare.backspaceCompare("a##c", "#a#c"));
+        assertTrue(backspaceStringCompare.backspaceCompare("a##c", "#a#c"));
     }
 
 }
