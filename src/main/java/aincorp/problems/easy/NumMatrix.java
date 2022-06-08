a5f2c2983597e070781dd849d3d92620cae73db2
Анваров Алмаз Салимович (4060794) <asanvarov@vtb.ru>
2022-06-09 00:14:13 +0300

    304. Range Sum Query 2D - Immutable

---
+++ src/main/java/aincorp/problems/easy/NumMatrix.java
@@ -0,0 +1,28 @@
+package aincorp.problems.easy;
+
+/**
+ * 304. Range Sum Query 2D - Immutable
+ * <p>
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
+public class NumMatrix {
+    private int[][] matrix = null;
+
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int summary = 0;
+
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                summary += matrix[i][j];
+            }
+        }
+
+        return summary;
+    }
+}
---
+++ src/test/java/aincorp/problems/easy/NumMatrixTest.java
@@ -0,0 +1,23 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumMatrixTest {
+    private int[][] input = new int[][]{
+            {3, 0, 1, 4, 2},
+            {5, 6, 3, 2, 1},
+            {1, 2, 0, 1, 5},
+            {4, 1, 0, 1, 7},
+            {1, 0, 3, 0, 5}};
+    private NumMatrix numMatrix = new NumMatrix(input);
+
+    @Test
+    public void test() {
+        assertEquals(8, numMatrix.sumRegion(2, 1, 4, 3));
+        assertEquals(11, numMatrix.sumRegion(1, 1, 2, 2));
+        assertEquals(12, numMatrix.sumRegion(1, 2, 2, 4));
+    }
+
+}
