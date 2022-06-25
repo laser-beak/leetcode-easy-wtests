b7cdee7437cd11c1b0ec2561653cd45f4854100b
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-06-25 15:17:06 +0300

    1332. Remove Palindromic Subsequences

---
+++ src/main/java/aincorp/problems/easy/RemovePalindromicSubsequences.java
@@ -0,0 +1,20 @@
+package aincorp.problems.easy;
+
+public class RemovePalindromicSubsequences {
+    public int removePalindromeSub(String s) {
+        return isPalindrome(s) ? 1 : 2;
+    }
+
+    boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/aincorp/problems/easy/RemovePalindromicSubsequencesTest.java
@@ -0,0 +1,31 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemovePalindromicSubsequencesTest {
+
+    private RemovePalindromicSubsequences palindromicSubsequences = new RemovePalindromicSubsequences();
+
+    @Test
+    public void s_is_palindrome() {
+        assertEquals(1, palindromicSubsequences.removePalindromeSub("ababa"));
+    }
+
+    @Test
+    public void s_is_one_Symbol() {
+        assertEquals(1, palindromicSubsequences.removePalindromeSub("a"));
+    }
+
+    @Test
+    public void s_has_two_palindrome() {
+        assertEquals(2, palindromicSubsequences.removePalindromeSub("abb"));
+    }
+
+    @Test
+    public void s_has_palindrome_answer_two() {
+        assertEquals(2, palindromicSubsequences.removePalindromeSub("baabb"));
+    }
+
+}
