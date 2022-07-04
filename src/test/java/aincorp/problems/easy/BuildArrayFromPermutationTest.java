29fb10fe966033dd5c3bd1e638eed3267f892801
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-07-04 23:32:11 +0300

    1920. Build Array from Permutation

---
+++ src/test/java/aincorp/problems/easy/BuildArrayFromPermutationTest.java
@@ -0,0 +1,17 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class BuildArrayFromPermutationTest {
+
+    private final BuildArrayFromPermutation buildArrayFromPermutation = new BuildArrayFromPermutation();
+
+    @Test
+    public void test() {
+        assertArrayEquals(new int[]{0, 1, 2, 4, 5, 3}, buildArrayFromPermutation.buildArray(new int[]{0, 2, 1, 5, 3, 4}));
+        assertArrayEquals(new int[]{4, 5, 0, 1, 2, 3}, buildArrayFromPermutation.buildArray(new int[]{5, 0, 1, 2, 3, 4}));
+    }
+
+}
