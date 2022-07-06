424f6950fb516cd367bef65db18e2b0a9173f5bd
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-07-06 19:36:38 +0300

    1588. Sum of All Odd Length Subarrays

---
+++ src/test/java/aincorp/problems/easy/SumOfAllOddLengthSubarraysTest.java
@@ -0,0 +1,18 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SumOfAllOddLengthSubarraysTest {
+
+    private final SumOfAllOddLengthSubarrays sum = new SumOfAllOddLengthSubarrays();
+
+    @Test
+    public void test() {
+        assertEquals(3, sum.sumOddLengthSubarrays(new int[]{1,2}));
+        assertEquals(66, sum.sumOddLengthSubarrays(new int[]{10, 11, 12}));
+        assertEquals(58, sum.sumOddLengthSubarrays(new int[]{1, 4, 2, 5, 3}));
+    }
+
+}
