09c62da5e49d0405406ef6a5127c7b5a1d3e6e8b
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-07-06 23:41:39 +0300

    1913. Maximum Product Difference Between Two Pairs

---
+++ src/test/java/aincorp/problems/easy/MaximumProductDifferenceBetweenTwoPairsTest.java
@@ -0,0 +1,17 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MaximumProductDifferenceBetweenTwoPairsTest {
+
+    private final MaximumProductDifferenceBetweenTwoPairs pairs = new MaximumProductDifferenceBetweenTwoPairs();
+
+    @Test
+    public void test() {
+        assertEquals(34, pairs.maxProductDifference(new int[]{5, 6, 2, 7, 4}));
+        assertEquals(64, pairs.maxProductDifference(new int[]{4, 2, 5, 9, 7, 4, 8}));
+    }
+
+}
