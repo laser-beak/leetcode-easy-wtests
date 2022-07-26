17e745c17a60f5ec731fa21f41ba434ccee434b9
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-07-26 20:33:58 +0300

    1534. Count Good Triplets

---
+++ src/test/java/aincorp/problems/easy/CountGoodTripletsTest.java
@@ -0,0 +1,17 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CountGoodTripletsTest {
+
+    private CountGoodTriplets countGoodTriplets = new CountGoodTriplets();
+
+    @Test
+    public void test() {
+        assertEquals(0, countGoodTriplets.countGoodTriplets(new int[]{1, 1, 2, 2, 3}, 0, 0, 1));
+        assertEquals(4, countGoodTriplets.countGoodTriplets(new int[]{3, 0, 1, 1, 9, 7}, 7, 2, 3));
+    }
+
+}
