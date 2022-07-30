99a960b074781cbbf2c91cfda9b4230c67be0182
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-07-31 00:45:37 +0300

    2108. Find First Palindromic String in the Array

---
+++ src/main/java/aincorp/problems/easy/FindFirstPalindromicStringInTheArray.java
@@ -0,0 +1,24 @@
+package aincorp.problems.easy;
+
+public class FindFirstPalindromicStringInTheArray {
+
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    private boolean isPalindrome(String word) {
+        int wordLength = word.length();
+        for (int i = 0; i < wordLength / 2; i++) {
+            if (word.charAt(i) != word.charAt(wordLength - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/aincorp/problems/easy/FindFirstPalindromicStringInTheArrayTest.java
@@ -0,0 +1,19 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FindFirstPalindromicStringInTheArrayTest {
+
+    private final FindFirstPalindromicStringInTheArray find = new FindFirstPalindromicStringInTheArray();
+
+    @Test
+    public void test() {
+        assertEquals("", find.firstPalindrome(new String[]{}));
+        assertEquals("ada", find.firstPalindrome(new String[]{"abc", "car", "ada", "racecar", "cool"}));
+        assertEquals("racecar", find.firstPalindrome(new String[]{"notapalindrome", "racecar"}));
+        assertEquals("", find.firstPalindrome(new String[]{"def", "ghi"}));
+    }
+
+}
