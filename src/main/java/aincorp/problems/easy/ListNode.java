0fe4eea69bc88a48d555c5c859197e18ed0c9c9f
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-07-31 18:22:48 +0300

    237. Delete Node in a Linked List

--- src/main/java/aincorp/problems/easy/ListNode.java
+++ src/main/java/aincorp/problems/easy/ListNode.java
@@ -1,5 +1,7 @@
 package aincorp.problems.easy;
 
+import java.util.Objects;
+
 public class ListNode {
     int val;
     ListNode next;
@@ -14,5 +16,18 @@ public class ListNode {
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode listNode = (ListNode) o;
+        return val == listNode.val && Objects.equals(next, listNode.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, next);
     }
 }
