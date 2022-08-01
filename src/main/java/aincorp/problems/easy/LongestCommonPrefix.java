63cce28ed55ff6f11ce54416368ec0a9941d3bfb
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-08-01 18:54:55 +0300

    14. Longest Common Prefix

--- src/main/java/aincorp/problems/easy/LongestCommonPrefix.java
+++ src/main/java/aincorp/problems/easy/LongestCommonPrefix.java
@@ -1,17 +1,53 @@
 package aincorp.problems.easy;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class LongestCommonPrefix {
 
     public String longestCommonPrefix(String[] strs) {
-        Set<String> words = new HashSet<>();
+
         String longestCommonPrefix = "";
 
-        String word = strs[0];
+        if (strs.length == 1) {
+            longestCommonPrefix = strs[0];
+        }
 
+        int minLength = strs[0].length();
+        int index = 0;
+        for (int i = 1; i < strs.length; i++) {
+            if (minLength > strs[i].length()) {
+                minLength = strs[i].length();
+                index = i;
+            }
+        }
+        String str = strs[index];
+
+        int i = 1;
+
+        while (i <= str.length()) {
+            String sub = str.substring(0, i);
+            boolean flag = false;
+            for (int j = 0; j < strs.length; j++) {
+                if (strs[j].substring(0, i).equals(sub)) {
+                    flag = true;
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                longestCommonPrefix = sub;
+            }
+            i++;
+        }
 
         return longestCommonPrefix;
+
+        /*if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;*/
     }
 }
---
+++ src/test/java/aincorp/problems/easy/LongestCommonPrefixTest.java
@@ -0,0 +1,19 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LongestCommonPrefixTest {
+
+    private final LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+
+    @Test
+    public void test() {
+        assertEquals("", longestCommonPrefix.longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+        assertEquals("fl", longestCommonPrefix.longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+        assertEquals("a", longestCommonPrefix.longestCommonPrefix(new String[]{"a"}));
+        assertEquals("", longestCommonPrefix.longestCommonPrefix(new String[]{"reflower", "flow", "flight"}));
+    }
+
+}
