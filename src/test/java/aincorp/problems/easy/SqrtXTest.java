51e09fb460f08b3f21958c36dd888cf036644928
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-09-07 23:26:45 +0300

    69. Scrt(x)

---
+++ src/test/java/aincorp/problems/easy/SqrtXTest.java
@@ -0,0 +1,19 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SqrtXTest {
+
+    private final SqrtX sqrtX = new SqrtX();
+
+    @Test
+    public void test() {
+        assertEquals(0, sqrtX.mySqrt(0));
+        assertEquals(2, sqrtX.mySqrt(4));
+        assertEquals(2, sqrtX.mySqrt(8));
+        assertEquals(3, sqrtX.mySqrt(9));
+        assertEquals(46339, sqrtX.mySqrt(2147395599));
+    }
+}
