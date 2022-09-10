ba7296579ddf98c32465d2d38be1273c690121e9
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-09-10 23:28:16 +0300

    412. Fizz Buzz

---
+++ src/test/java/aincorp/problems/easy/FizzBuzzTest.java
@@ -0,0 +1,59 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    private final FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    public void test_first() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("1");
+        stringList.add("2");
+        stringList.add("Fizz");
+
+        assertTrue(stringList.equals(fizzBuzz.fizzBuzz(3)));
+    }
+
+    @Test
+    public void test_second() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("1");
+        stringList.add("2");
+        stringList.add("Fizz");
+        stringList.add("4");
+        stringList.add("Buzz");
+
+        assertTrue(stringList.equals(fizzBuzz.fizzBuzz(5)));
+    }
+
+    @Test
+    public void test_third() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("1");
+        stringList.add("2");
+        stringList.add("Fizz");
+        stringList.add("4");
+        stringList.add("Buzz");
+        stringList.add("Fizz");
+        stringList.add("7");
+        stringList.add("8");
+        stringList.add("Fizz");
+        stringList.add("Buzz");
+
+        stringList.add("11");
+        stringList.add("Fizz");
+        stringList.add("13");
+        stringList.add("14");
+        stringList.add("FizzBuzz");
+
+        assertTrue(stringList.equals(fizzBuzz.fizzBuzz(15)));
+    }
+
+}
