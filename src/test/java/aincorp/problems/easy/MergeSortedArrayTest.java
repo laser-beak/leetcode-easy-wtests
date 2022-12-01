85c1e8822c7a07fc430d703e502e8e677e33a3a2
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2022-12-01 22:54:22 +0300

    88. Merge Sorted Array

--- src/test/java/aincorp/problems/easy/MergeSortedArrayTest.java
+++ src/test/java/aincorp/problems/easy/MergeSortedArrayTest.java
@@ -1,6 +1,5 @@
 package aincorp.problems.easy;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -13,9 +12,18 @@ class MergeSortedArrayTest {
         assertArrayEquals(new int[]{1}, mergeSortedArray.merge(new int[]{1}, 1, new int[]{}, 0));
     }
 
-    @Disabled
+    @Test
+    public void test_when_m_is_zero_nums2_empty() {
+        assertArrayEquals(new int[]{1}, mergeSortedArray.merge(new int[]{0}, 0, new int[]{1}, 1));
+    }
+
     @Test
     public void test() {
-        assertArrayEquals(new int[]{1, 2, 2, 3, 5, 6}, mergeSortedArray.merge(new int[]{1, 2, 3, 0, 0, 0}, 6, new int[]{2, 5, 6}, 3));
+        assertArrayEquals(new int[]{1, 2, 2, 3, 5, 6}, mergeSortedArray.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3));
+    }
+
+    @Test
+    public void test_nums1_is_zeroes() {
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, mergeSortedArray.merge(new int[]{0, 0, 0, 0, 0}, 0, new int[]{1,2,3,4,5}, 5));
     }
 }
