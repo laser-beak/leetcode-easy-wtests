40b7fe42c7b006cc1886de073d6ec095fe8606b0
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2023-01-07 12:41:43 +0300

    144. Binary Tree Preorder Traversal

---
+++ src/main/java/aincorp/problems/easy/BinaryTreePreorderTraversal.java
@@ -0,0 +1,51 @@
+package aincorp.problems.easy;
+
+import aincorp.problems.easy.models.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+public class BinaryTreePreorderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> integers = new ArrayList<>();
+
+        if (root == null) return integers;
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode current = root;
+        stack.push(current);
+
+        while (!stack.empty()) {
+            current = stack.pop();
+            integers.add(current.val);
+
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+
+        return integers;
+    }
+}
---
+++ src/test/java/aincorp/problems/easy/BinaryTreePreorderTraversalTest.java
@@ -0,0 +1,21 @@
+package aincorp.problems.easy;
+
+import aincorp.problems.easy.models.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BinaryTreePreorderTraversalTest {
+
+    private final BinaryTreePreorderTraversal binaryTreePreorderTraversal = new BinaryTreePreorderTraversal();
+
+    @Test
+    public void test() {
+        assertEquals(Arrays.asList(), binaryTreePreorderTraversal.preorderTraversal(null));
+        assertEquals(Arrays.asList(1), binaryTreePreorderTraversal.preorderTraversal(new TreeNode(1)));
+        assertEquals(Arrays.asList(1, 2, 3), binaryTreePreorderTraversal.preorderTraversal(new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null))));
+    }
+
+}
