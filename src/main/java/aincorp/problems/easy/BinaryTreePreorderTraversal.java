590f22807500762f34022723545fca9876ea5b2d
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2023-01-07 12:44:28 +0300

    144. Binary Tree Preorder Traversal

--- src/main/java/aincorp/problems/easy/BinaryTreePreorderTraversal.java
+++ src/main/java/aincorp/problems/easy/BinaryTreePreorderTraversal.java
@@ -48,4 +48,21 @@ public class BinaryTreePreorderTraversal {
 
         return integers;
     }
+
+    /*public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> list = new ArrayList<>();
+        preOrder(root, list);
+        return list;
+    }
+
+    public void preOrder(TreeNode node, List<Integer> list) {
+        if (node == null)
+            return;
+
+        list.add(node.val);
+        preOrder(node.left, list);
+        preOrder(node.right, list);
+
+    }*/
 }
