74fae94db00fcab607af0d4a32395c858774a2c2
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2023-01-31 15:11:04 +0300

    2535. Difference Between Element Sum and Digit Sum of an Array

---
+++ src/main/java/aincorp/problems/easy/DifferenceBetweenElementSumAndDigitSumOfAnArray.java
@@ -0,0 +1,20 @@
+package aincorp.problems.easy;
+
+public class DifferenceBetweenElementSumAndDigitSumOfAnArray {
+
+    public int differenceOfSum(int[] nums) {
+
+        int elementSum = 0;
+        int digitSum = 0;
+
+        for (int num : nums) {
+            elementSum += num;
+
+            while (num > 0) {
+                digitSum += num % 10;
+                num = num / 10;
+            }
+        }
+        return Math.abs(elementSum - digitSum);
+    }
+}
---
+++ src/test/java/aincorp/problems/easy/DifferenceBetweenElementSumAndDigitSumOfAnArrayTest.java
@@ -0,0 +1,17 @@
+package aincorp.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DifferenceBetweenElementSumAndDigitSumOfAnArrayTest {
+
+    private DifferenceBetweenElementSumAndDigitSumOfAnArray difference = new DifferenceBetweenElementSumAndDigitSumOfAnArray();
+
+    @Test
+    public void test() {
+        assertEquals(9, difference.differenceOfSum(new int[]{1, 15, 6, 3}));
+        assertEquals(0, difference.differenceOfSum(new int[]{1, 2, 3, 4}));
+    }
+
+}
