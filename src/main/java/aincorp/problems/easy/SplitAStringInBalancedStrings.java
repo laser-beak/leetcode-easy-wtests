99deabf39f4b3778d6ee6bb49772a51df44ec672
Anvarov Almaz Salimovich <anvarov.almaz@yandex.ru>
2023-11-07 16:04:24 +0300

    1221. Split a String in Balanced Strings

--- src/main/java/aincorp/problems/easy/SplitAStringInBalancedStrings.java
+++ src/main/java/aincorp/problems/easy/SplitAStringInBalancedStrings.java
@@ -3,18 +3,21 @@ package aincorp.problems.easy;
 public class SplitAStringInBalancedStrings {
 
     public int balancedStringSplit(String s) {
-        int result = 0;
-
-        if (s.length() == 2) {
-            return 1;
-        }
+        int count = 0;
+        int countRL = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'R' && s.charAt(i + 1) == 'L') {
-                result++;
+            if (s.charAt(i) == 'R') {
+                countRL++;
+            }
+            if (s.charAt(i) == 'L') {
+                countRL--;
+            }
+            if (countRL == 0) {
+                count++;
             }
         }
 
-        return result;
+        return count;
     }
 }
--- src/test/java/aincorp/problems/easy/SplitAStringInBalancedStringsTest.java
+++ src/test/java/aincorp/problems/easy/SplitAStringInBalancedStringsTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SplitAStringInBalancedStringsTest {
-
     private final SplitAStringInBalancedStrings splitAStringInBalancedStrings = new SplitAStringInBalancedStrings();
 
     @Test
@@ -13,6 +12,9 @@ class SplitAStringInBalancedStringsTest {
         assertEquals(0, splitAStringInBalancedStrings.balancedStringSplit(""));
         assertEquals(1, splitAStringInBalancedStrings.balancedStringSplit("RL"));
         assertEquals(2, splitAStringInBalancedStrings.balancedStringSplit("RLRL"));
+        assertEquals(2, splitAStringInBalancedStrings.balancedStringSplit("LRLR"));
+        assertEquals(4, splitAStringInBalancedStrings.balancedStringSplit("RLRRLLRLRL"));
+        assertEquals(1, splitAStringInBalancedStrings.balancedStringSplit("LLLLRRRR"));
+        assertEquals(2, splitAStringInBalancedStrings.balancedStringSplit("RLRRRLLRLL"));
     }
-
 }
